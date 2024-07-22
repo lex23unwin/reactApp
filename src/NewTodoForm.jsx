a5972168d80711845ec17ae279f9a5f0bc606a85
lex23unwin <lex23unwin@gmail.com>
2024-07-22 16:25:58 -0400

    using props to pass in addTodo functionality to child component

---
+++ src/NewTodoForm.jsx
@@ -0,0 +1,24 @@
+import { useState } from 'react'
+
+export function NewTodoForm() {
+
+    const [newItem, setNewItem] = useState("");
+
+    function handleSubmit(e) {
+        e.preventDefault()
+    
+        addTodo(newItem)
+    
+        setNewItem("")
+      }
+
+    return (
+        <form onSubmit={handleSubmit} className="new-item-form"> 
+          <div className="form-row">
+            <label htmlFor="item">New Item</label>
+            <input value={newItem} type="text" id="item" onChange={e => setNewItem(e.target.value)}/>
+          </div>
+          <button className="btn">Add</button>
+        </form>
+    )
+}
