e2b0cefb64caaeee162c3b4308d3aa98538dc36d
lex23unwin <lex23unwin@gmail.com>
2024-07-22 14:48:39 -0400

    Todo List

--- src/App.js
+++ src/App.js
@@ -4,10 +4,31 @@ import React from 'react';
 export default function App() {
 
   return (
-    <>
-      <div>
-        <h1>Hi</h1>
-      </div>
-    </>
+      <>
+        <form className="new-item-form"> 
+          <div className="form-row">
+            <label htmlFor="item">New Item</label>
+            <input type="text" id="item" />
+          </div>
+        <button className="btn">Add</button>
+        </form>
+        <h1 className="header">Todo List</h1>
+        <ul className="list">
+          <li>
+            <label>
+              <input type="checkbox" />
+              Item 1
+            </label>
+            <button className="btn btn-danger">Delete</button>
+          </li>
+          <li>
+            <label>
+              <input type="checkbox" />
+              Item 2
+            </label>
+            <button className="btn btn-danger">Delete</button>
+          </li>
+        </ul>
+      </>
   );
 }
